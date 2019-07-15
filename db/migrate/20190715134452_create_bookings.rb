6f084a5c2316b6cd13322546e08e998a2a4dcb2d
Bruno Mazzilli <brunoma94@yahoo.es>
2019-07-15 15:47:26 +0200

    migrate booking model

--- db/migrate/20190715134452_create_bookings.rb
+++ db/migrate/20190715134452_create_bookings.rb
@@ -3,7 +3,7 @@ class CreateBookings < ActiveRecord::Migration[5.2]
     create_table :bookings do |t|
       t.date :date
       t.date :time
-      t.boolean :confirmed
+      t.boolean :confirmed?, default: false
       t.references :user, foreign_key: true
       t.references :shower, foreign_key: true
 
