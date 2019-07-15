f02fb9e4512ef1f2cf7e1050207cd1dae9c0efe7
Adrian Gutierrez Alonso <ingenieria.aga@gmail.com>
2019-07-15 15:29:25 +0200

    Migrate shower model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_131158) do
+ActiveRecord::Schema.define(version: 2019_07_15_132738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "showers", force: :cascade do |t|
+    t.text "description"
+    t.string "address"
+    t.text "features"
+    t.integer "price"
+    t.date "availability"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_showers_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -30,4 +42,5 @@ ActiveRecord::Schema.define(version: 2019_07_15_131158) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "showers", "users"
 end
