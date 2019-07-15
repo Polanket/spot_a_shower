8c68ba2f7c2fd23a19aee444b0ff7b471995c9b2
Adrian Gutierrez Alonso <ingenieria.aga@gmail.com>
2019-07-15 16:57:12 +0200

    Create Showers controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'showers/index'
+  get 'showers/show'
+  get 'showers/new'
+  get 'showers/create'
+  get 'showers/edit'
+  get 'showers/update'
+  get 'showers/delete'
   devise_for :users
   root to: 'showers#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
