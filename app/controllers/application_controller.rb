709747f65d664243f834d6cd2b05944703253f4c
Juan <juanmpt@outlook.com>
2019-07-16 11:44:23 +0200

    Add application controller logic to take user to same page after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_user_location!, if: :storable_location?
   before_action :authenticate_user!
 
   protected
@@ -12,7 +13,20 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :owner?])
   end
 
-  def after_sign_out_path_for(resource)
+  def after_sign_out_path_for(resource_or_scope)
     root_path
+  end
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
   end
 end
