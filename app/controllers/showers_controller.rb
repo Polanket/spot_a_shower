e8f661676dcf2b7ae52a218f4f8eddab77854df1
Juan <juanmpt@outlook.com>
2019-07-16 15:54:43 +0200

    Added shower title to the permitted parameters

--- app/controllers/showers_controller.rb
+++ app/controllers/showers_controller.rb
@@ -44,7 +44,7 @@ class ShowersController < ApplicationController
   end
 
   def shower_params
-    params.require(:shower).permit(:description, :address, :features, :price)
+    params.require(:shower).permit(:title, :description, :address, :features, :price)
   end
 
   def set_shower
