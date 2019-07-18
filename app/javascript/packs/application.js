1b4c3001dcce487f41db3a7f7e39013695d0b083
Juan <juanmpt@outlook.com>
2019-07-18 15:55:27 +0200

    Add js animation for homepage banner

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,5 +5,8 @@ import { initMapbox } from '../plugins/init_mapbox';
 
 import { initAutocomplete } from '../plugins/init_autocomplete'
 
+import { typeAnimation } from '../plugins/init_typed'
+
 initMapbox();
 initAutocomplete();
+typeAnimation();
