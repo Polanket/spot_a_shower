69e6c42c3c1545e32e09a274563304a17222f5be
Juan <juanmpt@outlook.com>
2019-07-18 10:56:52 +0200

    Showers migration to add lat long coordinates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_17_102338) do
+ActiveRecord::Schema.define(version: 2019_07_18_085619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2019_07_17_102338) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.string "photo"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_showers_on_user_id"
   end
 
